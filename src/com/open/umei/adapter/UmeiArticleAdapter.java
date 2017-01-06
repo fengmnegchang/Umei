5e8aa7423bf65e87b4d01132e15750275f2fdc33
fengmnegchang <624926379@qq.com>
2017-01-06 10:08:59 +0800

    listview adapter图片设置原始大小，不采用复用 Signed-off-by: fengmnegchang <624926379@qq.com>

--- src/com/open/umei/adapter/UmeiArticleAdapter.java
+++ src/com/open/umei/adapter/UmeiArticleAdapter.java
@@ -22,6 +22,7 @@ import android.widget.ImageView;
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 import com.open.umei.R;
 import com.open.umei.activity.m.UmeiMActicleViewPagerActivity;
 import com.open.umei.bean.UmeiArticleBean;
@@ -40,45 +41,82 @@ import com.open.umei.json.UmeiArticleJson;
  */
 public class UmeiArticleAdapter extends CommonAdapter<UmeiArticleBean> {
 	public String url;
-	public UmeiArticleAdapter(Context mContext, List<UmeiArticleBean> list,String url) {
+
+	public UmeiArticleAdapter(Context mContext, List<UmeiArticleBean> list, String url) {
 		super(mContext, list);
 		this.url = url;
 	}
-//	public UmeiArticleAdapter(Context mContext, List<UmeiArticleBean> list ) {
-//		super(mContext, list);
-//	}
+
+	// public UmeiArticleAdapter(Context mContext, List<UmeiArticleBean> list )
+	// {
+	// super(mContext, list);
+	// }
+	// @Override
+	// public View getView(int position, View convertView, ViewGroup parent) {
+	// final UmeiArticleBean bean = (UmeiArticleBean) getItem(position);
+	// ViewHolder mViewHolder;
+	// if (convertView == null) {
+	// mViewHolder = new ViewHolder();
+	// convertView =
+	// LayoutInflater.from(mContext).inflate(R.layout.adapter_umei_article,
+	// null);
+	// mViewHolder.imageview = (ImageView)
+	// convertView.findViewById(R.id.imageview);
+	//
+	// convertView.setTag(mViewHolder);
+	// } else {
+	// mViewHolder = (ViewHolder) convertView.getTag();
+	// }
+	//
+	// if (bean != null) {
+	// if (bean.getSrc() != null && bean.getSrc().length() > 0) {
+	// DisplayImageOptions options = new
+	// DisplayImageOptions.Builder().showStubImage(R.drawable.common_v4).showImageForEmptyUri(R.drawable.common_v4).showImageOnFail(R.drawable.common_v4)
+	// .cacheInMemory().cacheOnDisc().imageScaleType(ImageScaleType.EXACTLY_STRETCHED).build();
+	// ImageLoader.getInstance().displayImage(bean.getSrc(),
+	// mViewHolder.imageview, options, getImageLoadingListener());
+	// }
+	//
+	// mViewHolder.imageview.setOnClickListener(new OnClickListener() {
+	// @Override
+	// public void onClick(View v) {
+	// // UmeiWebViewActivity.startUmeiWebViewActivity(mContext, bean.getSrc());
+	// UmeiArticleJson mUmeiArticleJson = new UmeiArticleJson();
+	// mUmeiArticleJson.setList(list);
+	// UmeiMActicleViewPagerActivity.startUmeiMActicleViewPagerActivity(mContext,
+	// mUmeiArticleJson,url);
+	// }
+	// });
+	// }
+	// return convertView;
+	// }
+
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		final UmeiArticleBean bean = (UmeiArticleBean) getItem(position);
-		ViewHolder mViewHolder;
-		if (convertView == null) {
-			mViewHolder = new ViewHolder();
-			convertView = LayoutInflater.from(mContext).inflate(R.layout.adapter_umei_article, null);
-			mViewHolder.imageview = (ImageView) convertView.findViewById(R.id.imageview);
 
-			convertView.setTag(mViewHolder);
-		} else {
-			mViewHolder = (ViewHolder) convertView.getTag();
-		}
+		View view = LayoutInflater.from(mContext).inflate(R.layout.adapter_umei_article, null);
+		ImageView imageview = (ImageView) view.findViewById(R.id.imageview);
 
 		if (bean != null) {
 			if (bean.getSrc() != null && bean.getSrc().length() > 0) {
 				DisplayImageOptions options = new DisplayImageOptions.Builder().showStubImage(R.drawable.common_v4).showImageForEmptyUri(R.drawable.common_v4).showImageOnFail(R.drawable.common_v4)
-						.cacheInMemory().cacheOnDisc().build();
-				ImageLoader.getInstance().displayImage(bean.getSrc(), mViewHolder.imageview, options, getImageLoadingListener());
+						.cacheInMemory().cacheOnDisc().imageScaleType(ImageScaleType.EXACTLY_STRETCHED).build();
+				ImageLoader.getInstance().displayImage(bean.getSrc(), imageview, options, getImageLoadingListener());
 			}
-			
-			mViewHolder.imageview.setOnClickListener(new OnClickListener() {
+
+			imageview.setOnClickListener(new OnClickListener() {
 				@Override
 				public void onClick(View v) {
-//					UmeiWebViewActivity.startUmeiWebViewActivity(mContext, bean.getSrc());
+					// UmeiWebViewActivity.startUmeiWebViewActivity(mContext,
+					// bean.getSrc());
 					UmeiArticleJson mUmeiArticleJson = new UmeiArticleJson();
 					mUmeiArticleJson.setList(list);
-					UmeiMActicleViewPagerActivity.startUmeiMActicleViewPagerActivity(mContext, mUmeiArticleJson,url);
+					UmeiMActicleViewPagerActivity.startUmeiMActicleViewPagerActivity(mContext, mUmeiArticleJson, url);
 				}
 			});
 		}
-		return convertView;
+		return view;
 	}
 
 	private class ViewHolder {
